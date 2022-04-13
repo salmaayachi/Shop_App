dc5954b80fd797cbc042cf4bdfcf14ea3f1359e7
selma <salma.ayachi@insat.u-carthage.tn>
2022-04-13 15:18:42 +0100

    updating with error

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -119,9 +119,19 @@ class Products with ChangeNotifier {
     }
   }
 
-  void updateProduct(String? id, Product newProduct) {
+  Future<void> updateProduct(String? id, Product newProduct) async {
     final prodIndex = _items.indexWhere((prod) => prod.id == id);
+
     if (prodIndex >= 0) {
+      final url = Uri.parse(
+          'https://flutter-shop-app-c7469-default-rtdb.firebaseio.com/products/$id.json');
+      await http.patch(url,
+          body: json.encode({
+            'title': newProduct.title,
+            'description': newProduct.description,
+            'imageUrl': newProduct.imageUrl,
+            'price': newProduct.price
+          }));
       _items[prodIndex] = newProduct;
       notifyListeners();
     } else {
